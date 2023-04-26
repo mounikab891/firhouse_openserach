5d83e6d39736c740a44d822b19d0aef6f7ec7dc9
Mounika Bethu <113983011+mounikab891@users.noreply.github.com>
2023-04-26 13:03:48 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,38 @@
+variable "vpc" {
+    default = "vpc-08e222910f114ade8"
+}
+
+variable "security_group_id" {
+    default = "sg-XXXXXX"
+}
+variable "aws_subnet" {
+    default = "subnet-xxxxxxxx"
+}
+
+variable "index_name" {
+    default = "stg-xxxxxxxx"
+}
+
+variable "firehose_name" {
+    default = "stg-xxxxxxxx"
+}
+
+variable "domain_name" {
+    default = "stg-xxxxxxxx"
+}
+variable "bucket_arn" {
+    default = "arn:aws:s3:::s3-xxxxxxxx"
+}
+variable "opensearch_domain_arn" {
+    default = "arn:aws:es:xxxxxxxx:xxxxxxxx"
+}
+ 
+variable "s3_prefix" {
+    default = "stg-xxxxxxxx"
+}
+variable "bucket" {
+    default = "s3-xxxxxxxx"
+}
+variable "key" {
+    default = "stg-xxxxxxxx/"
+}
